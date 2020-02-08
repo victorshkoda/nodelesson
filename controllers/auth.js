79517a16e3e431a97987fdaed7324d709b9f1273
victorshkoda <victorshkoda@gmail.com>
2020-02-09 01:38:12 +0600

    Added jsonwebtoken

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const bcript = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const authmodel = require('../models/Auth');
 const codeError = require('../config/code_errors');
+const key = require('../config/key');
 
 module.exports.users = async function (req, res) {
     const users = await authmodel.getUsers();
@@ -11,10 +13,14 @@ module.exports.login = async function (req, res) {
     const email = req.body.email.trim();
     const password = req.body.password.trim();
     const user = await authmodel.getUser(email);
+    const token = jwt.sign({
+        email: user[0].email,
+        id: user[0].id
+    }, key.jwt, {expiresIn: 2592000 });
     const ifPassword = bcript.compareSync(password, user[0].passwd);
     if(user.length > 0 && ifPassword){
-        if(!req.cookies.token)res.cookie('token', Math.random());
-        res.status(200).json({user})
+        if(!req.cookies.token)res.cookie('token', token);
+        res.status(200).json({user, token})
     }else{
         res.status(200).json({'message': codeError[10002], "code": "10002"});
     }
