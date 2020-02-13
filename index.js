5cd91d713831ad37ea7f9803ab513f67e1e3edf7
victor <cloudtokarevia@gmail.com>
2020-02-13 13:50:49 +0600

    Added socket

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const app = require('./app');
 const http = require('http').createServer(app);
 const port = process.env.PORT || 3030;
+const io = require('socket.io')(http);
+const {sock} = require('./utils/socket');
+
+io.on('connection', socket => {
+    sock(socket);
+    console.log('Socket started');
+});
+
 http.listen(port, () => console.log(`started on port ${port}`));
---
+++ utils/socket.js
@@ -0,0 +1,15 @@
+const authmodel = require('../models/Auth');
+
+module.exports.sock = (socket) => {
+    socket.on('login', async (data) => {
+        console.log(data);
+        let sendUser = {message: "Not found user"};
+        const user = await authmodel.getUser(data);
+        if(user.length > 0){
+            sendUser = user[0];
+        }
+        socket.emit('status', sendUser);
+    })
+};
+
+
